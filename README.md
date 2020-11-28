21c963887e066858d05ccc85bb2394beb469518e
oksil joo <oksiljoo@oksilui-MacBook-Pro.local>
2020-11-28 03:17:22 -0500

    setting up packages

--- README.md
+++ README.md
@@ -1,45 +1,17 @@
-## Rock Paper Scissors - Python text-base take on a classic!
+# Rock Paper Scissors in Python!
 
-Pick your weapon and play against the computer - you'll only get a virtual sting if you lose!
+## Pick your weapon and play against the computer - you'll only get a virtual sting if you lose!
 
-## Prerequisites
+Prerequisites: All you need is [Python 3](https://www.python.org/) to play.
 
-All you need is Python 3 to play.
+Clone the project, navigate to the correct directory- co directory name - and run the game.py file in a Python 3 Terminal window for macOS / Linux
 
-## Usage
+The rules are the same as the classic game:
+1. paper covers Rock
+2. Scissors cuts Paper 3.Rock samshes Scissors
 
-Clone the project, <code>cd</code> to your directory/download, and run the game.py file in a Python 3 shell (Terminal on a mac / Linux, Windows command prompt).
+Choose you weapon wisely!
 
-## Rules
+The computer choose first, just follow the prompts.
 
-same as the classic game:
-
-paer covers Rock Scissors cuts Paper Rock smashes Scissors
-
-Choose your weapon wisely!
-
-Built with Python 3
-
-Enjoy!
-
-## License
-MIT License
-Copyright (c) 2020 Oksil-Joo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Built with [Python 3](https://www.python.org/)
--- game.py
+++ game.py
@@ -1,127 +1,79 @@
 # inport packages to extend python (just like we extend subline, or Atom, or VSCode)
 from random import randint
 
-# [] => this is an array. an array is a special type of container that can hold multipe itmes
-# arrays are indexed (their contents get assigned a number)
-# The index alwyas starts at 0
-choices = ["rock", "paper", "scissors"]
-
-
-player_lives = 1
-AI_lives = 1
-
-total_lives = 1
-
-player = False;
-
-# define a win or lose funtion
-def winorlose(status):
-	# print("inside winorlose function; status is: ", status)
-
-	#print("You", status, "! Would you like to play again?")
-	#choice = input("Y / N? ")
-	if status == "won":
-		pre_message = "You are the Yuuuuuuuuuugest winner ever! "
-	else:
-		pre_message = "You done trumped it, loser! "
-	
-	print(pre_message + "Would you like to play again? ")
-	
-	choice = input("Y / N? ")
-
-	if choice == "Y" or choice == "y":
-		
-		global player_lives
-		global AI_lives
-		global player
-
-		player_lives = 1
-		AI_lives = 1
-		player = False
-
-	
-	elif choice == "N" or choice == "n":
-		# exit messae and quit
-		print("You choose to quit! Better luck next time!")
-		exit()
-
-	else:
-		print("Make a valid choice - Y or N")
-		#this will generate a bug that we need to fix later
-		choice = input("Y / N: ")
+# re-import our game variables
+from gameComponents import gameVars, winLose
 
 # set up our game loop
-while player is False:
+while gameVars.player is False:
 	print("=====================*/ RPS GAME /*======================")
-	print("Computer Lives:", AI_lives, "/", total_lives)
-	print("player Lives:", player_lives, "/", total_lives)
+	print("Computer Lives:", gameVars.computer_lives, "/", gameVars.total_lives)
+	print("gameVars.player Lives:", gameVars.player_lives, "/", gameVars.total_lives)
 	print("=========================================================")
 
 	print("Choose your weapon! or type quit to exit\n") #\n means "new line"
 	# this is the player choice
-	player = input("choose rock, paper or scissors: \n")
+	gameVars.player = input("choose rock, paper or scissors: \n")
 
 	# if the player chose to quit, then exit the game
-	if player == "quit":
+	if gameVars.player == "quit":
 		print("you chose to quit, quitter!")
 		exit()
 
 	# player = true -> it has a value (rock, paper, or scissors)
 
 	# this will be the AI choice -> a random pick from the choices array
-	computer = choices[randint(0, 2)]
+	computer = gameVars.choices[randint(0, 2)]
 
 	# check to see what the user input
 
 	# print outputs whatever is in the round brackets -> in this case it outputs player to the command prompt window
-	print("user chose: " + player)
+	print("user chose: " + gameVars.player)
 
 	# validate that the random choice worked for the AI
 	print("AI chose: " + computer)
 
-	if (computer == player):
+	if (computer == gameVars.player):
 		print("tie")
 
 	# always check for negative conditions first (the losing case)
 	elif(computer == "rock"):
-		if(player == "scissors"):
-			player_lives = player_lives - 1
-			print("you lose! player lives: ", player_lives)
+		if(gameVars.player == "scissors"):
+			gameVars.player_lives -= 1
+			print("you lose! player lives: ", gameVars.player_lives)
 		else:
 			print("you win!")
-			AI_lives = AI_lives - 1
+			gameVars.computer_lives -= 1
 
 	elif (computer == "paper"):
-		if (player == "scissors"):
-			player_lives = player_lives - 1
-			print("you lose! player lives: ", player_lives)
+		if (gameVars.player == "scissors"):
+			gameVars.player_lives -= 1
+			print("you lose! gameVars.player lives: ", gameVars.player_lives)
 		else:
 			print("you win!")
-			AI_lives = AI_lives - 1
+			gameVars.computer_lives -= 1
 
 	elif (computer == "scissors"):
-		if (player == "rock"):
-			player_lives = player_lives - 1
-			print("you lose! player lives: ", player_lives)
+		if (gameVars.player == "rock"):
+			gameVars.player_lives -= 1
+			print("you lose! gameVars.player lives: ", gameVars.player_lives)
 		else:
 			print("you win!")
-			AI_lives = AI_lives - 1
+			gameVars.computer_lives -= 1
 	
-	if player_lives == 0:
-		winorlose("lost")
+	if gameVars.player_lives == 0:
+		winLose.winorlose("lost")
 
-		
-
-	elif AI_lives == 0:
-		winorlose("won")
+	elif gameVars.computer_lives == 0:
+		winLose.winorlose("won")
 
 	else:
-		player = False
+		gameVars.player = False
 
 
-	print("player has:", player_lives, "lives left")
-	print("AI has:", AI_lives, "Lives left")
+	print("gameVars.player has:", gameVars.player_lives, "lives left")
+	print("AI has:", gameVars.computer_lives, "Lives left")
 
-	# make the loop keep running by setting player back to False
+	# make the loop keep running by setting gameVars.player back to False
 	# unset, so that our loop condition above will evaluate to True
-	player = False
+	gameVars.player = False
